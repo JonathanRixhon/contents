03f2bba2ffb7aff1d096bfb94015300989290cd5
Jonathan Rixhon <jonathan.rixhon@hotmail.com>
2024-12-30 08:20:58 +0100

    Add table to ManageContents

--- src/Filament/Resources/PageResource/Pages/ManageContents.php
+++ src/Filament/Resources/PageResource/Pages/ManageContents.php
@@ -2,15 +2,22 @@
 
 namespace Jonathanrixhon\Contents\Filament\Resources\PageResource\Pages;
 
+use Filament\Tables\Table;
 use Jonathanrixhon\Contents\Filament\Resources\PageResource;
+use Jonathanrixhon\Contents\Filament\Resources\ContentResource;
 use Jonathanrixhon\Contents\Filament\Pages\ManageContents as ManageContentsBase;
 
 class ManageContents extends ManageContentsBase
 {
     protected static string $resource = PageResource::class;
-    
+
     public static function getNavigationLabel(): string
     {
         return __('contents::label.contents');
+    }
+    
+    public function table(Table $table): Table
+    {
+        return ContentResource::table($table);
     }
 }
