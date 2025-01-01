5e04bb10a44956b29a58af4f7a5706691602b399
Jonathan Rixhon <jonathan.rixhon@hotmail.com>
2025-01-01 11:55:07 +0100

    fix translations

--- src/Filament/Resources/PageResource/Pages/ManageContents.php
+++ src/Filament/Resources/PageResource/Pages/ManageContents.php
@@ -10,14 +10,4 @@ use Jonathanrixhon\Contents\Filament\Pages\ManageContents as ManageContentsBase;
 class ManageContents extends ManageContentsBase
 {
     protected static string $resource = PageResource::class;
-
-    // public static function getNavigationLabel(): string
-    // {
-    //     return __('contents::fields.contents.label');
-    // }
-
-    // public function table(Table $table): Table
-    // {
-    //     return ContentResource::table($table);
-    // }
 }
